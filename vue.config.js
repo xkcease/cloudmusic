8770bc9bd09722d1b7a897312d557f4b77e0707a
xkcease <xkcease@163.com>
2020-12-05 10:47:21 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,17 @@ module.exports = {
                 }
             }
         }
+    },
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '/'
+                },
+                // secure: false
+            }
+        }
     }
-}; 
+};
